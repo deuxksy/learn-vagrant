803955f2da78c6ecdd545743208ce31deb8d7c41
SeokYoung.Kim <deuxksy@gmail.com>
2021-07-24 14:43:35 +0900

    kube join 자동화 하기

--- shell/bootstrap-node.sh
+++ shell/bootstrap-node.sh
@@ -2,3 +2,5 @@
 
 source <(kubectl completion bash)
 echo "source <(kubectl completion bash)" >> ~/.bashrc
+
+kubeadm join --token 123456.1234567890123456 --discovery-token-unsafe-skip-ca-verification 192.168.1.81:6443
